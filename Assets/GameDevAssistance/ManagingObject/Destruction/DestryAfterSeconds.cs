83a03c1c2cd2faad5d3c852c177e429dab2fcaea
Jihwan Park <jhpark8904@gmail.com>
2016-12-21 17:36:05 +0900

    feat: Add Object destroy script
    Add C# script that can destroy object after specific seconds

---
+++ Assets/GameDevAssistance/ManagingObject/Destruction/DestryAfterSeconds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestryAfterSeconds : MonoBehaviour {
+
+    public float Time;
+
+    //Count down from start and destroy object.
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(Time);
+        Destroy(gameObject);
+    }
+}
